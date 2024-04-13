bba364ce301d78a4aa5d8660f05a198cbabedd1b
josedeleon01 <josealbertodeleon8@gmail.com>
2024-04-13 16:17:20 -0400

    Identation
    Identation

--- ProductManagementSystem.Client/Pages/Login/Login.razor.cs
+++ ProductManagementSystem.Client/Pages/Login/Login.razor.cs
@@ -4,29 +4,29 @@ using ProductManagementSystem.Client.Interfaces.Users;
 
 namespace ProductManagementSystem.Client.Pages.Login
 {
-	public partial class Login
-	{
-		[Inject]
-		public IUserService AuthService { get; set; }
+    public partial class Login
+    {
+        [Inject]
+        public IUserService AuthService { get; set; }
 
-		[Inject]
-		public NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
 
-		private AuthenticateRequest model = new();
-		private string errorMessage;
+        private AuthenticateRequest model = new();
+        private string errorMessage;
 
-		private async Task SubmitAsync()
-		{
-			try
-			{
-				await AuthService.Authenticate(model);
-				NavigationManager.NavigateTo("/reports/customeritems");
-				errorMessage = null;
-			}
-			catch (Exception ex)
-			{
-				errorMessage = ex.Message;
-			}
-		}
-	}
+        private async Task SubmitAsync()
+        {
+            try
+            {
+                await AuthService.Authenticate(model);
+                NavigationManager.NavigateTo("/reports/customeritems");
+                errorMessage = null;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+    }
 }
