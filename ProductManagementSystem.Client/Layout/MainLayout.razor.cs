a821ae3abd2eee99215f61a349f6c78d325c0b7e
josedeleon01 <josealbertodeleon8@gmail.com>
2024-04-17 09:12:33 -0400

    Adding LogOut
    Adding LogOut

--- ProductManagementSystem.Client/Layout/MainLayout.razor.cs
+++ ProductManagementSystem.Client/Layout/MainLayout.razor.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.AspNetCore.Components;
-using ProductManagementSystem.Client.Services.ComponentState;
+using ProductManagementSystem.Client.Interfaces.Users;
 
 namespace ProductManagementSystem.Client.Layout
 {
     public partial class MainLayout
     {
+        [Inject] 
+        IUserService userService { get; set; }
+
         private bool collapseNavMenu { get; set; } = false;
-        private string NavMenuCssClass { get; set; } 
+
+        private string NavMenuCssClass { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
         private void ToggleNavMenu()
         {
             NavMenuCssClass = !collapseNavMenu ? "hide" : string.Empty;
@@ -15,6 +22,14 @@ namespace ProductManagementSystem.Client.Layout
 
             // Notify Blazor to re-render the component
             StateHasChanged();
+
+        }
+
+        public async Task LogOut()
+        {
+             await userService.LogoutAsync();
+            NavigationManager.NavigateTo("/login");
+
 
         }
     }
